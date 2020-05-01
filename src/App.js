28b78fd1aeca3a8ac0f1f49e35a79a0937f6d46f
alex <barmaleyalex@gmail.com>
2020-05-01 13:09:31 -0700

    created general layout, made cards and added some styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import Cards from './componenets/Cards/Cards';
+// import Chart from './componenets/Chart/Chart';
+// import CountryPicker from './componenets/CountryPicker/CountryPicker';
+import {Cards,Chart,CountryPicker} from './componenets';
+import {fetchData} from './api'
+import styles from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+       data:{}
+    };  
+}
+
+
+async componentDidMount(){
+  const data = await fetchData();
+  console.log(data);
+  this.setState({data:data})
+
+}
+
+
+
+
+  render(){
+    const {data} = this.state;
+
+
+    return(
+      <div className={styles.container}>
+
+        <Cards  data={data}/>
+        <CountryPicker />
+        <Chart />
+
+      </div>
+    )
+  }
+ 
 }
 
 export default App;
